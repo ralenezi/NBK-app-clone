1334e917ae7dd2464332bc487341b3151babba19
Rahaf Alenezi <irahaf@live.it>
2021-06-25 18:57:18 +0300

    burger button was added

--- src/components/Home/Hi.js
+++ src/components/Home/Hi.js
@@ -1,7 +1,7 @@
-import { makeStyles } from '@material-ui/core'
+import { makeStyles, AppBar, Toolbar, IconButton } from '@material-ui/core'
+import { Menu } from '@material-ui/icons'
 import React from 'react'
 
-//   console.log('🚀 ~ file: Hi.js ~ line 6 ~ theme', theme)
 const useStyles = makeStyles((theme) => ({
   root: {
     backgroundColor: theme.palette.background.dark,
@@ -9,12 +9,28 @@ const useStyles = makeStyles((theme) => ({
     paddingBottom: theme.spacing(3),
     paddingTop: theme.spacing(3),
   },
+  menuButton: {
+    marginRight: theme.spacing(2),
+  },
+  title: {
+    flexGrow: 1,
+  },
 }))
 const Hi = () => {
   const classes = useStyles()
   return (
-    <div>
-      <h1 className={classes.root}>hiii!!</h1>
+    <div className={classes.root}>
+      <AppBar position='static'>
+        <Toolbar>
+          <IconButton
+            edge='start'
+            className={classes.menuButton}
+            color='inherit'
+            aria-label='menu'>
+            <Menu />
+          </IconButton>
+        </Toolbar>
+      </AppBar>
     </div>
   )
 }

a0c9e93be904a0e87d11351e1ad7415fdb756a03
Rahaf Alenezi <irahaf@live.it>
2021-06-25 13:17:27 +0300

    Initialize project using Create React App

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React from "react"
+import "./App.css"
+
+// Routes
+import Routes from "./routes";
+
+const App = () => {
+  return (
+    <div className="wrapper">
+      <Routes />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/routes/index.js
@@ -0,0 +1,15 @@
+import React from "react";
+import { Route, Switch } from "react-router";
+
+// Components
+import Home from "../components/Home"
+
+const Routes = () => {
+  return (
+    <Switch>
+      <Route exact component={Home} path="/" />
+    </Switch>
+  );
+};
+
+export default Routes;
